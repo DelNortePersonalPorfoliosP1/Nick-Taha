4a1ca6fdb4b5c52589a230813ae0809bfbed8e5d
Nick <nicktahavs@gmail.com>
2019-09-13 09:01:56 -0700

    Create main.c

---
+++ NicKTahaLab1/NicKTahaLab1/main.c
@@ -0,0 +1,25 @@
+//
+//  main.c
+//  NicKTahaLab1
+//
+//  Created by Taha, Nick on 9/13/19.
+//  Copyright © 2019 Taha, Nick. All rights reserved.
+//
+
+#include <stdio.h>
+#include <math.h>
+
+int main(int argc, const char * argv[]) {
+    double a,b;
+    
+    printf("Enter side value a: \n");
+    scanf("%lf", &a);
+    printf("Enter side value b: \n");
+    scanf("%lf", &b);
+    double c = sqrt(a*a+b*b);
+    
+    printf("Your C value is: \n");
+    printf("%lf\n",c);
+    
+    return 0;
+}
